332c8bef9c9503ce05a042eb159830f825527696
sungjae <92013056+rlawo32@users.noreply.github.com>
2024-06-05 14:28:41 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import TempView from "./reference/tempView";
+
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <TempView />
     </main>
   );
 }
