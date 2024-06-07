1a59c44dc8e064b76de01e72ec3e4ef4e311fcc9
sungjae <92013056+rlawo32@users.noreply.github.com>
2024-06-07 14:53:55 +0900

    Update tempZoomIn.tsx

--- src/app/reference/tempZoomIn.tsx
+++ src/app/reference/tempZoomIn.tsx
@@ -14,6 +14,13 @@ const BtnSection = styled('div')<{$x:number, $y:number, $size:number}>`
 
     transform-origin: ${({$x}) => $x}px ${({$y}) => $y}px;
     transform: scale(${({$size}) => $size}, ${({$size}) => $size});
+    
+    // transition-property, transition duration, transition-timing-function, transition delay
+    // transition-property: transform, opacity; 타겟
+    // transition-duration: 0.5s, 0.5s; 동작시간
+    // transition-timing-function: ease-in, ease-in; 동작방식
+    // transition-delay: 0s, 0.5s; 딜레이
+    
     transition: transform 0.5s ease-in 0s, opacity 0.5s ease-in 0.5s;
     
     opacity: ${({$size}) => $size > 1 ? 0 : 1};
