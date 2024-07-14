603a2f59af6f660c486bd797a423b41bebb067b3
rlawo <rlawo32@naver.com>
2024-07-14 23:56:13 +0900

    [PopulationMap-project][FE] <API> KakaoMap_API_작업_20240714 (2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,15 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 import Nationwide from "./map/nationwide";
+import ReactKakaoMap from "./reference/reactKakaoMap";
+import TempView from "./reference/tempView";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Nationwide />
+      {/* <Nationwide /> */}
+      <ReactKakaoMap />
+      {/* <TempView /> */}
     </main>
   );
 }
