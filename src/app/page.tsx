463d4ecfdb71bba9d8f05326e77a93993af08b8c
rlawo <rlawo32@naver.com>
2024-09-29 03:43:03 +0900

    [PopulationMap-project][FE] <SSR> COMPONENT_분리_작업_20240929 (2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
-import Nationwide from "./map/nationwide";
-import ReactKakaoMap from "./reference/reactKakaoMap";
-import TempView from "./reference/tempView";
+import View from "./view/page";
+import Naionwide from "./map/nationwide";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Nationwide />
-      {/* <ReactKakaoMap /> */}
-      {/* <TempView /> */}
-    </main>
+    <div>
+      <View />
+    </div>
   );
 }
---
+++ src/app/view/page.tsx
@@ -0,0 +1,11 @@
+import ViewMain from "./view-main/page";
+import styles from "../page.module.css";
+
+export default function View() {
+    
+  return (
+    <div className={styles.view}>
+      <ViewMain />
+    </div>
+  );
+}
