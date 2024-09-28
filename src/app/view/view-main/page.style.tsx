3e22bb1db53b8ca9a41e3ff3e7434228c13c2f32
rlawo <rlawo32@naver.com>
2024-09-29 04:26:49 +0900

    [PopulationMap-project][FE] <CSS> MAIN_STYLE_작업_20240929 (7)

--- src/app/view/view-main/page.style.tsx
+++ src/app/view/view-main/page.style.tsx
@@ -11,7 +11,6 @@ export const ViewMain = styled('div')`
     .pop-left {
         height: 100%;
         width: 30%;
-        border: 2px solid red;
     }
 
     .pop-right {
@@ -19,14 +18,13 @@ export const ViewMain = styled('div')`
         flex-direction: column;
         height: 100%;
         width: 70%;
-        border: 1px solid red;
     
         .pop-right-top {
+            background-color: rgba(0, 127, 255, .3);
         }
 
         .pop-right-bottom {
             height: 350px;
-            border: 1px solid red;
         }
     }
 `;
