e7adafc65236267c2b16367d3e487aaf7c915b89
rlawo <rlawo32@naver.com>
2024-09-30 01:34:02 +0900

    [PopulationMap-project][FE] <SUPA> SUPABASE_작업_20240930 (5)

--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -1,7 +1,15 @@
 import ViewMain from "./view-main/page";
 import styles from "../page.module.css";
+import supabase from "../supabase";
 
 export default function View() {
+  const client:any = supabase();
+
+  const {data:population_jul, error} = client
+    .from("population_jul")
+    .select("pop_total")
+
+  console.log("test")
     
   return (
     <div className={styles.view}>
