6d74f2a58c11be7d382a1c28a27b7633bd2a7887
rlawo <rlawo32@naver.com>
2024-10-03 23:51:47 +0900

    [PopulationMap-project][FE] <VIEW> 숫자증가_작업_20241003 (1)

---
+++ src/app/view/view-sub1/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/navigation";
+
+const Nationwide = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Nationwide;
