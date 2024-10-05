4bbf9dbc8920bce1a84b1485bd877a319c6a4b26
rlawo <rlawo32@naver.com>
2024-10-06 03:07:53 +0900

    [PopulationMap-project][FE] <CHARTJS> 그래프_작업_20241006 (3)

---
+++ src/app/view/view-sub1/page.style.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ViewSub1 = styled('div')`
+    canvas {
+        position: relative;
+        height: 30vh !important;
+        width: 50vw !important;
+    }
+`;
--- src/app/view/view-sub1/page.tsx
+++ src/app/view/view-sub1/page.tsx
@@ -1,15 +1,57 @@
 'use client';
 
 import { useEffect, useRef, useState } from "react";
-import { useRouter } from "next/navigation";
 import supabase from "@/app/supabase";
 import useLocationDataStore from "@/app/stores/useLocationDataStore";
+import { Bar, Chart, Line } from "react-chartjs-2";
+import 'chart.js/auto';
+
+import * as Styled from "./page.style";
 
 const ViewSub1 = () => {
     const client:any = supabase();
 
     const {month, setMonth, column, setColumn} = useLocationDataStore();
     const [viewSub1Data, setViewSub1Data] = useState<number>(0);
+
+    const data = {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [15, 10, 5, 15, 10, 5],
+            backgroundColor: [
+                'red',
+                'blue',
+                'yellow',
+                'green',
+                'purple',
+                'orange',
+            ],
+            borderColor: [
+                'red',
+                'blue',
+                'yellow',
+                'green',
+                'purple',
+                'orange',
+            ],
+            borderWidth: 1,
+        }],
+    };
+
+    const options = {
+        responsive: true,
+        scales: {
+            y: {
+                min: 0,
+                max: 15,
+                ticks:{ 
+                    fontSize: 24,
+                    stepSize: 5
+                }
+            },
+        },
+    };
     
     useEffect(() => {
         const frameRate = 1000/60;
@@ -41,9 +83,9 @@ const ViewSub1 = () => {
     }, [])
 
     return (
-        <div>
-            {viewSub1Data}
-        </div>
+        <Styled.ViewSub1>
+            <Bar data={data} options={options} /> 
+        </Styled.ViewSub1>
     )
 }
 
