435c237bd45be8ff3f0f4f05765f24d62d0c5d1e
sungjae <92013056+rlawo32@users.noreply.github.com>
2024-10-11 16:54:32 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false // console 2번 찍히는 현상 해결
+    reactStrictMode: false // console.log 2번 찍히는 현상 해결
 };
 
 export default nextConfig;
