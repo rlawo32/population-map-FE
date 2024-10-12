3bfdf623c9d15e05a067e84ddc9dd43e8ca2a045
rlawo <rlawo32@naver.com>
2024-10-13 02:31:24 +0900

    [PopulationMap-project][FE] <VIEW> MAINVIEW_작업_20241013 (4)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import styles from "./page.module.css";
+import { Noto_Sans_KR } from "next/font/google";
 
 import View from "./view/page";
 import ViewMain from "./view/view-main/page";
 import Naionwide from "./map/nationwide";
 
+const noto = Noto_Sans_KR({
+  subsets: ['latin'],
+  weight: ['600']
+});
+
 export default function Home() {
   return (
     <div className={styles.main}>
-      <ViewMain />
+      <div className={noto.className}>
+        <ViewMain />
+      </div>
     </div>
   );
 }
