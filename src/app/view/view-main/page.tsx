122de0f9e86ef11cd7b8eb4bf4a24be709787d3d
rlawo <rlawo32@naver.com>
2024-10-13 02:31:31 +0900

    [PopulationMap-project][FE] <VIEW> MAINVIEW_작업_20241013 (5)

--- src/app/view/view-main/page.tsx
+++ src/app/view/view-main/page.tsx
@@ -2,12 +2,14 @@ import styles from "../../page.module.css";
 
 import Nationwide from "@/app/map/nationwide";
 import ViewSub1 from "../view-sub1/page";
+import ViewSub2 from "../view-sub2/page";
 
 const ViewMain = () => {
 
     return (
         <div className={styles.view}>
             <div className={styles.popLeft}>
+                <ViewSub2 />
             </div>
             <div className={styles.popRight}>
                 <div className={styles.popRightTop}>
