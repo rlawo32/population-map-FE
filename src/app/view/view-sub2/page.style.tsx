7756b4ab18f4c50d8ba58be4d827382d9b450c21
rlawo <rlawo32@naver.com>
2024-10-13 02:31:14 +0900

    [PopulationMap-project][FE] <VIEW> MAINVIEW_작업_20241013 (3)

--- src/app/view/view-sub2/page.style.tsx
+++ src/app/view/view-sub2/page.style.tsx
@@ -4,7 +4,35 @@ export const ViewSub2 = styled('div')`
     position: relative;
     height: 30vh;
     width: 100vw;
+    color: black;
 
-    canvas {
+    .vs_title {
+    
+    }
+
+    .vs_content {
+    
+    }
+
+    .vs_top {
+
+        .vs_location {
+        
+        }
+
+        .vs_total_default {
+            
+        }
+
+        .vs_total_gender {
+
+            .vs_total_m {
+            
+            }
+
+            .vs_total_w {
+            
+            }
+        }
     }
 `;
--- src/app/view/view-sub2/page.tsx
+++ src/app/view/view-sub2/page.tsx
@@ -13,6 +13,11 @@ const ViewSub2 = () => {
 
     const {month, setMonth, column, setColumn} = useLocationDataStore();
     const [viewSub1Data, setViewSub1Data] = useState<number>(0);
+
+    const [locationData, setLocationData] = useState<string>("");
+    const [totalData, setTotalData] = useState<number>(0);
+    const [mTotalData, setMTotalData] = useState<number>(0);
+    const [wTotalData, setWTotalData] = useState<number>(0);
     
     useEffect(() => {
         const frameRate = 1000/60;
@@ -44,7 +49,50 @@ const ViewSub2 = () => {
     }, [])
 
     return (
+        // ViewSub = vs
         <Styled.ViewSub2>
+            <div className="vs_top">
+                <div className="vs_location">
+                    <div className="vs_title">
+                        현재 지역
+                    </div>
+                    <div className="vs_content">
+                        {locationData}
+                    </div>
+                </div>
+                <div className="vs_total_default">
+                    <div className="vs_title">
+                        전체
+                    </div>
+                    <div className="vs_content">
+                        {totalData}
+                    </div>
+                </div>
+                <div className="vs_total_gender">
+                    <div className="vs_total_m">
+                        <div className="vs_title">
+                            남
+                        </div>
+                        <div className="vs_content">
+                            {mTotalData}
+                        </div>
+                    </div>
+                    <div className="vs_total_w">
+                        <div className="vs_title">
+                            여
+                        </div>
+                        <div className="vs_content">
+                            {wTotalData}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="vs_mid">
+
+            </div>
+            <div className="vs_bot">
+
+            </div>
         </Styled.ViewSub2>
     )
 }
