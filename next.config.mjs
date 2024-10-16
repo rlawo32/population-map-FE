0892b1b54b9cfd042095ba1ec75ec3bcdf214871
rlawo <rlawo32@naver.com>
2024-10-17 00:35:59 +0900

    [PopulationMap-project][FE] <SSR> CSR/SSR_클래스명오류_개선_작업_20241017 (1)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false // console.log 2번 찍히는 현상 해결
+    reactStrictMode: false, // console.log 2번 찍히는 현상 해결
+    compiler: {
+        // Warning: Prop className did not match. => 서버와 클라이언트에서 클래스명이 다르기에 나타나는 오류 해결
+        // Next.js는 첫 페이지 로드가 SSR로 동작하기 때문에, 
+        // 서버에서 생성된 컴포넌트와 클라이언트에서 생성된 컴포넌트의 클래스명이 서로 달라지게 된다.
+        // NextJS 12버전부터 babel 대신 swc를 사용하여 컴파일하도록 변경됨, 아래 코드 추가 시 해결
+        styledComponents: true, 
+    }
 };
 
 export default nextConfig;
