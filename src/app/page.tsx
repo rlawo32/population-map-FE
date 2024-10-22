b98647ef3da896cc4fc229922e105f37ea9a8cbe
rlawo <rlawo32@naver.com>
2024-10-22 22:34:29 +0900

    [PopulationMap-project][FE] <VIEW> 지도이동_작업_20241022 (1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import styles from "./page.module.css";
 import { Noto_Sans_KR } from "next/font/google";
 
-import View from "./view/page";
 import ViewMain from "./view/view-main/page";
-import Naionwide from "./map/nationwide";
 
 const noto = Noto_Sans_KR({
   subsets: ['latin'],
