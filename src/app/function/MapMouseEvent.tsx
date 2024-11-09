ed3ffc62d96112379c0c13e0a44904528aa803af
rlawo <rlawo32@naver.com>
2024-11-09 21:44:28 +0900

    [PopulationMap-project][FE] <VIEW> 지도이동_작업_20241109 (12)

---
+++ src/app/function/MapMouseEvent.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
+const MapMouseClick = (props:{num:number;}) => {
+    const [resultData, setResultData] = useState<number>(0);
+
+    return (
+        <div>
+        </div>
+    )
+}
+
+const MapMouseOver = (props:{num:number;}) => {
+    const [resultData, setResultData] = useState<number>(0);
+
+    return (
+        <div>
+        </div>
+    )
+}
+
+const MapMouseOut = (props:{num:number;}) => {
+    const [resultData, setResultData] = useState<number>(0);
+
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default MapMouseOver;
